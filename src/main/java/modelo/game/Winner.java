d0c85f090357e1d661fc4f5e8be9ce0275eee138
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2023-10-08 19:56:31 -0300

    refactor: removed propeties no usage

--- src/main/java/modelo/game/Winner.java
+++ src/main/java/modelo/game/Winner.java
@@ -6,19 +6,10 @@ import modelo.user.User;
 import java.util.ArrayList;
 
 public class Winner {
-    private final User user;
     private ArrayList<Field> winnerFields;
-
-    Winner(User user) {
-        this.user = user;
-    }
 
     public void setWinnerFields(ArrayList<Field> winnerFields) {
         this.winnerFields = winnerFields;
-    }
-
-    public User getUser() {
-        return user;
     }
 
     public ArrayList<Field> getWinnerFields() {
